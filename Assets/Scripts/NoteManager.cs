35a780f9a0df97bc3d920ea33b82c643b99aa194
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-11-06 04:17:28 +0300

    Make note image public

--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -3,19 +3,18 @@ using UnityEngine.UI;
 
 public class NoteManager : MonoBehaviour
 {
+    [Tooltip("Sprite renderer of the note")]
+    public Image image;
+    
     [Tooltip("Time when it's gonna be tapped by the player")]
     public float assignTime;
 
     [Tooltip("Time when it's gonna be instantiated")]
     private double _instantiateTime;
-    [Tooltip("Sprite renderer of the note")]
-    private Image _image;
 
     private void Start()
     {
         _instantiateTime = GameManager.GetAudioSourceTime();
-        
-        _image = GetComponent<Image>();
     }
 
     private void Update()
@@ -29,7 +28,7 @@ public class NoteManager : MonoBehaviour
             transform.localPosition = Vector3.Lerp(
                 Vector3.right * GameManager.Instance.spawnPosition,
                 Vector3.right * GameManager.Instance.despawnPosition, t);
-            _image.enabled = true;
+            image.enabled = true;
         }
     }
 }
