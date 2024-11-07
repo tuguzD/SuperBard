9e551d0eecbdf232d47e3600bf1375a12dd60400
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-11-07 23:08:38 +0300

    Add some timings for primary drum set of "Return To Slime"

--- Assets/Project/Scenes/Return To Slime/RtS_Pri_DrumSet_LaneManager.cs
+++ Assets/Project/Scenes/Return To Slime/RtS_Pri_DrumSet_LaneManager.cs
@@ -1,45 +1,42 @@
 using UnityEngine;
 
 // ReSharper disable once InconsistentNaming
-[AddComponentMenu("Scripts/Tubular Bell Lane (Secondary for 'The Tale of a Cruel World')")]
-public class TToaCW_Sec_TubBell_LaneManager : LaneManager
+[AddComponentMenu("Scripts/Drum Set Lane (Primary for 'Return Of Slime')")]
+public class RtS_Pri_DrumSet_LaneManager : LaneManager
 {
     private float _storedPriority;
-
+    
     private new void Start()
     {
         var delay = gameManager.startDelay;
         base.Start();
-
+    
         Invoke(nameof(RemoveBasicPriority),
-            delay + 0);
-
+            delay + 51f);
         Invoke(nameof(AddBackBasicPriority),
-            delay + 45);
-        Invoke(nameof(RemoveBasicPriority),
-            delay + 70);
-
-        Invoke(nameof(AddBackBasicPriority),
-            delay + 145);
-        Invoke(nameof(RemoveBasicPriority),
-            delay + 170);
+            delay + 97.5f);
+        
+        // Invoke(nameof(RemoveBasicPriority),
+        //     delay + 51f);
+        // Invoke(nameof(AddBackBasicPriority),
+        //     delay + 97.5f);
     }
-
+    
     private void RemoveBasicPriority()
     {
         _storedPriority = priorityModifier;
-        priorityModifier = 0;
-
-        Debug.LogWarning(
+        priorityModifier = 0.75f;
+    
+        Debug.LogError(
             $"Priority removed, but stored as {_storedPriority}");
     }
-
+    
     private void AddBackBasicPriority()
     {
         priorityModifier = _storedPriority;
         _storedPriority = 0;
-
-        Debug.LogWarning(
+    
+        Debug.LogError(
             $"Priority added back, cleared in class to be {_storedPriority}");
     }
 }
