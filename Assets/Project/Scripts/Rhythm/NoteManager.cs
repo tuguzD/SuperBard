dc7bc90d818f62d797ff30934729da279ca072fa
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-11-08 14:03:52 +0300

    Make note gray when its lane priority is less than 1

--- Assets/Project/Scripts/Rhythm/NoteManager.cs
+++ Assets/Project/Scripts/Rhythm/NoteManager.cs
@@ -5,22 +5,24 @@ public class NoteManager : MonoBehaviour
 {
     [Tooltip("Sprite renderer of the note")]
     public Image image;
-    
+    private Color _imageColor;
+
     [Tooltip("Time when it's gonna be tapped by the player")]
     public float assignTime;
 
     [Tooltip("Whether the note is colliding with the crosshair")]
     [HideInInspector] public bool isColliding;
     public float actualScale = 0.5f;
-    
+
     [Tooltip("Time when it's gonna be instantiated")]
     private double _instantiateTime;
 
     [HideInInspector] public LaneManager lane;
-    
+
     private void Start()
     {
         _instantiateTime = GameManager.GetAudioSourceTime();
+        _imageColor = image.color;
     }
 
     private void Update()
@@ -36,8 +38,16 @@ public class NoteManager : MonoBehaviour
                 Vector3.right * GameManager.Instance.despawnPosition, t);
             image.enabled = true;
         }
-        
+
         transform.localScale = Vector3.Lerp(
             transform.localScale, Vector3.one * (actualScale * lane.priorityModifier), Time.deltaTime * 5);
+
+        if (lane.priorityModifier >= 1) 
+            image.color = _imageColor;
+        else
+        {
+            _imageColor = image.color;
+            image.color = new Color(.5f, .5f, .5f, _imageColor.a);
+        }
     }
 }
