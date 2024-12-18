9353c9c342f7ab153a86b4f0d65d22b450be3ea9
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-12-18 13:57:06 +0300

    Play sound effects indicating player's quality of performance (tests only aim and does not rely on combo's and total score for now)

--- Assets/Project/Scripts/Narrative/Audience.cs
+++ Assets/Project/Scripts/Narrative/Audience.cs
@@ -3,11 +3,35 @@ using UnityEngine;
 
 public class Audience : MonoBehaviour
 {
+    private void ListenToMusic()
+    {
+        _happiness.PassiveDynamics.Type = QuantityDynamicsType.Depletion;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.TryGetComponent(typeof(Bullet), out var bullet)) return;
 
         _happiness.Amount += 0.25f;
+    }
+
+    [Header("Audio clips to play at the end")]
+    [SerializeField] private AudioClip cheering;
+    [SerializeField] private AudioClip worried;
+    [SerializeField] private AudioClip angry;
+
+    private void PlayAudio()
+    {
+        _happiness.PassiveDynamics.Type = QuantityDynamicsType.None;
+
+        var source = GetComponent<AudioSource>();
+        source.clip = _happiness.FillAmount switch
+        {
+            >= 0.75f => cheering,
+            <= 0.25f => angry,
+            _ => worried
+        };
+        source.Play();
     }
 
     private QuantityBhv _happiness;
@@ -17,9 +41,7 @@ public class Audience : MonoBehaviour
         _happiness = transform.parent.GetComponentInChildren<QuantityBhv>();
 
         var gameManager = FindObjectOfType<GameManager>();
-        gameManager.StartAction += () =>
-            _happiness.PassiveDynamics.Type = QuantityDynamicsType.Depletion;
-        gameManager.EndAction += () =>
-            Debug.Log("Cheer or cry out");
+        gameManager.StartAction += ListenToMusic;
+        gameManager.EndAction += PlayAudio;
     }
 }
